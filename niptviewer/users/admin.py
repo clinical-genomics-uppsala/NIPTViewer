47dbd17e401805e1fbf587353bc8dae46fbabe9e
Patrik Smeds <patrik.smeds@gmail.com>
2020-11-03 12:29:50 +0100

    Add user app.

---
+++ niptviewer/users/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
+# Unregister the provided model admin
+admin.site.unregister(User)
+
+class CustomUserAdmin(UserAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        disabled_fields = set()  # type: Set[str]
+        if not is_superuser:
+            disabled_fields |= {
+                'username',
+                'is_superuser',
+                'user_permissions',
+                'last_login',
+                'date_joined'
+            }
+
+        for f in disabled_fields:
+            if f in form.base_fields:
+                form.base_fields[f].disabled = True
+
+        return form
+
+admin.site.register(User, CustomUserAdmin)
