a4aac110a91643a467001ca37c1ce59b4b2c27da
Patrik Smeds <patrik.smeds@gmail.com>
2020-11-05 11:17:56 +0100

    Added missing js file.

---
+++ niptviewer/reportvisualiser/static/run_visualiser/js/grouped_barchart.js
@@ -0,0 +1,33 @@
+function horizontalGroupedBarChart({data,id}) {
+    nv.addGraph(function() { 
+      chart = nv.models.multiBarChart()
+            //.barColor(d3.scale.category20().range())
+            .duration(300)
+            .stacked(true)
+            .x(function(d) { return d.label })
+            .margin({bottom: 100, left: 70})
+            .rotateLabels(45)
+            .groupSpacing(0.1);
+
+        chart.reduceXTicks(false).staggerLabels(true);
+
+        chart.xAxis
+            .axisLabel("Sample")
+            .axisLabelDistance(35)
+            .showMaxMin(false)
+            .tickFormat(function(d) { return d;});
+
+        chart.yAxis
+            .axisLabel("Reads")
+            .axisLabelDistance(-5)
+            .tickFormat(d3.format('.1%'));
+
+        d3.select(id)
+            .datum(data)
+            .call(chart);
+
+        nv.utils.windowResize(chart.update);
+
+    return chart;
+    });
+};
