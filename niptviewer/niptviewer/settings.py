f20a5b15cf878bf4cdd2abb15599e2d78f453b81
Patrik Smeds <patrik.smeds@gmail.com>
2022-03-23 12:00:14 +0100

    chore: dev to master (#136)
    * Update release-please.yaml
    
    * fix: move ARG to after From (#131)
    
    * Update Dockerfile
    
    * Update docker-publish.yaml
    
    * Update docker-publish.yaml
    
    * Update docker-publish.yaml
    
    * Update docker-publish.yaml
    
    * Update docker-publish.yaml
    
    * Update docker-publish.yaml
    
    * Update docker-publish.yaml
    
    * Update docker-publish.yaml
    
    * Update docker-publish.yaml
    
    * Update docker-publish.yaml
    
    * Update docker-publish.yaml
    
    * refactor: clean up (#134)
    
    * refactor: remove extra '<' (#135)
    
    * refactor: remove extra '<'

--- niptviewer/niptviewer/settings.py
+++ niptviewer/niptviewer/settings.py
@@ -193,47 +193,6 @@ DEFAULT_FROM_EMAIL = os.environ.get('FROM_EMAIL', "DUMMAY")
 
 SERVER_EMAIL = os.environ.get('SERVER_EMAIL', "DUMMY")
 
-
-def get_secrets(variable, default):
-    if os.path.isfile(os.getenv(variable, default)):
-        data = ""
-        with open(os.getenv(variable), "r") as myfile:
-            data = myfile.readlines()
-        return data
-    else:
-        return os.getenv(os.getenv(variable, default))
-
-
-if os.environ.get('DATABASE', "sqlite3") == "postgres":
-    DATABASES = {
-        "default": {
-            "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-            "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-            "USER": os.environ.get("SQL_USER", "user"),
-            "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-            "HOST": os.environ.get("SQL_HOST", "localhost"),
-            "PORT": os.environ.get("SQL_PORT", "5432"),
-        }
-    }
-elif os.environ.get('DATABASE', "sqlite3") == "mssql":
-    DATABASES = {
-        "default": {
-            "ENGINE": 'mssql',
-            "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-            "USER": get_secrets("SQL_USER", "user"),
-            "PASSWORD": get_secrets("SQL_PASSWORD", "password"),
-            "HOST": os.environ.get("SQL_HOST", "localhost"),
-            "PORT": os.environ.get("SQL_PORT", ""),
-            "OPTIONS": {
-                'driver': 'ODBC Driver 17 for SQL Server',
-                'host_is_server': True,
-                'connection_timeout': 30,
-                'collation': 'SQL_Latin1_General_CP1_CI_AS',
-                'extra_params': 'Trusted_Connection=yes;Encrypt=yes',
-            }
-        }
-    }
-
 LOGGING_CONFIG = None
 #
 # # Get loglevel from env
